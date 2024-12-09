cbcb55e9843abb4349d729b1e7448866b6bf7831
Syed Sufiyan Ameen <65315541+syedsufyansufi@users.noreply.github.com>
2024-12-09 22:45:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# TodoList-CRUD-Js
-todolist crud app using only html css js
+# To-Do-list_CRUD-in-JavaScript
+The TODO List (CRUD) is a simple project developed using JavaScript, CSS, and HTML. The user can add the number of to-do work details and you can see the details stored in the list form. Also, you can even update the details of the record if you want. The user can delete the list of items if he/she wants to remove them.  
+
+Making of the Project
+This mini crud operation project is simply in HTML, CSS, and JavaScript. Talking about the features of this system, the user can make a list of their daily work and keep them as records. You just have to type the work information in the text fields. Then, click on the add button to add the information to the record. This project includes a lot of javascript for making the functioning of the project.
+
+How To Run the Project?
+To run this project, you don’t need to have any kind of local server but yet a browser. I recommend you use modern browsers like Google Chrome and Mozilla Firefox. To run this system, first, open the project in your browser by clicking the index.html file.
---
+++ script.js
@@ -0,0 +1,122 @@
+let toDoInput;
+let errorInfo;
+let addBtn;
+let ulList;
+let newToDo;
+
+let popup;
+let popupInfo;
+let todoToEdit;
+let popupInput;
+let popupAddBtn;
+let popupCloseBtn;
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  toDoInput = document.querySelector(".todo-input");
+  errorInfo = document.querySelector(".error-info");
+  addBtn = document.querySelector(".btn-add");
+  ulList = document.querySelector(".todolist ul");
+
+  popup = document.querySelector(".popup");
+  popupInfo = document.querySelector(".popup-info");
+  popupInput = document.querySelector(".popup-input");
+  popupAddBtn = document.querySelector(".accept");
+  popupCloseBtn = document.querySelector(".cancel");
+};
+
+const prepareDOMEvents = () => {
+  addBtn.addEventListener("click", addNewToDo);
+  ulList.addEventListener("click", checkClick);
+  popupCloseBtn.addEventListener("click", closePopup);
+  popupAddBtn.addEventListener("click", changeTodoText);
+  toDoInput.addEventListener("keyup", enterKeyCheck);
+};
+
+const addNewToDo = () => {
+  if (toDoInput.value != "") {
+    newToDo = document.createElement("li");
+    newToDo.textContent = toDoInput.value;
+    createToolAreal();
+    ulList.append(newToDo);
+
+    toDoInput.value = "";
+    errorInfo.textContent = "";
+  } else {
+    errorInfo.textContent = "EMPTY!";
+  }
+};
+
+const createToolAreal = () => {
+  const div = document.createElement("div");
+  div.classList.add("tools");
+  newToDo.append(div);
+
+  const buttonDone = document.createElement("button");
+  buttonDone.classList.add("complete");
+  buttonDone.innerHTML = '<i class="fas fa-check"></i>';
+
+  const buttonEdit = document.createElement("button");
+  buttonEdit.classList.add("edit");
+  buttonEdit.textContent = "EDIT";
+
+  const buttonCancel = document.createElement("button");
+  buttonCancel.classList.add("delete");
+  buttonCancel.innerHTML = '<i class="fas fa-times"></i>';
+
+  div.append(buttonDone, buttonEdit, buttonCancel);
+};
+
+const checkClick = (e) => {
+  if (e.target.matches(".complete")) {
+    e.target.closest("li").classList.toggle("completed");
+    e.target.classList.toggle("completed");
+  } else if (e.target.matches(".edit")) {
+    editToDo(e);
+  } else if (e.target.matches(".delete")) {
+    deleteToDo(e);
+  }
+};
+
+const editToDo = (e) => {
+  todoToEdit = e.target.closest("li");
+  popupInput.value = todoToEdit.firstChild.textContent;
+  popup.style.display = "flex";
+};
+
+const closePopup = () => {
+  popup.style.display = "none";
+  popupInfo.textContent = "";
+};
+
+const changeTodoText = () => {
+  if (popupInput.value != "") {
+    todoToEdit.firstChild.textContent = popupInput.value;
+
+    popup.style.display = "none";
+    popupInfo.textContent = "";
+  } else {
+    popupInfo.textContent = "Musisz podać jakąś treść!";
+  }
+};
+
+const deleteToDo = (e) => {
+  e.target.closest("li").remove();
+
+  const allToDos = ulList.querySelectorAll("li");
+  if (allToDos.length == 0) {
+    errorInfo.textContent = "EMPTY!";
+  }
+};
+
+const enterKeyCheck = (e) => {
+  if (e.key == "Enter") {
+    addNewToDo();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", main);
